f435d7373f75fba3baaa156ce0ec0a722abb7213
jlmunoz44 <jlmunoz4.miners@gmail.com>
2018-06-06 01:10:35 -0400

    added messy login toggle, clean later

--- AuctionClient/src/app/navigation/navigation.component.ts
+++ AuctionClient/src/app/navigation/navigation.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  btnTextShow: string = 'Login';
+  btnTextHide: string = 'User Login';
+
+  btnText: string = this.btnTextHide;
+  toggle(): void{
+    if (this.btnText === this.btnTextShow){
+      this.btnText = this.btnTextHide;
+    }else{
+      this.btnText = this.btnTextShow;
+    }
+  }
   constructor() { }
 
   ngOnInit() {
